232fb8d5792de03f9e6455779153b8952937eddd
Yadier <yadierg7@gmail.com>
2023-07-18 17:26:20 -0400

    first commit

---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+import { MouseEventHandler } from "react";
+
+interface Props {
+  text: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  styles: string;
+}
+const Button = ({ text, onClick, styles }: Props) => {
+  return (
+    <button className={styles} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+export default Button;
